60a05eca3250035745d811bb357485bbc5e6bd69
George Ng'ethe <gitzblitz@users.noreply.github.com>
2019-04-11 22:48:55 +0200

    Update quote data

--- app/src/main/java/com/gitzblitz/motivationalapp/controller/QuoteData.kt
+++ app/src/main/java/com/gitzblitz/motivationalapp/controller/QuoteData.kt
@@ -13,34 +13,34 @@ import org.json.JSONException
  * Created by george.ngethe on 28/03/2018.
  */
 class QuoteData {
-  lateinit var  quoteArrayList: ArrayList<Quote >
+    lateinit var quoteArrayList: ArrayList<Quote>
 
-  fun getQuotes(callback: QuoteListAsyncResponse): Unit {
-      val url = "https://raw.githubusercontent.com/pdichone/UIUX-Android-Course/master/Quotes.json%20"
+    fun getQuotes(callback: QuoteListAsyncResponse): Unit {
+        val url = "https://raw.githubusercontent.com/pdichone/UIUX-Android-Course/master/Quotes.json%20"
 
-      val quoteRequest = JsonArrayRequest(Request.Method.GET, url, Response.Listener { response: JSONArray ->
-          try {
+        val quoteRequest = JsonArrayRequest(Request.Method.GET, url, Response.Listener { response: JSONArray ->
+            try {
 
-              for (i in 0 until response.length()) {
-                  var quoteObject = response.getJSONObject(i)
-                  var quote = Quote(quoteObject.getString("quote"), quoteObject.getString("name"))
+                for (i in 0 until response.length()) {
+                    var quoteObject = response.getJSONObject(i)
+                    var quote = Quote(quoteObject.getString("quote"), quoteObject.getString("name"))
 //                  Log.d("Quotes:", quote.author)
-                  quoteArrayList.add(quote)
+                    quoteArrayList.add(quote)
 
-              }
-              
+                }
 
-          } catch (e: JSONException) {
-              e.printStackTrace()
-          }
-          if (true) callback.processFinished(quoteArrayList)
-      }, Response.ErrorListener { error: VolleyError ->
-          try {
-              Log.d("Error", "Not Working")
-          } catch (e: JSONException) {
-              e.printStackTrace()
-          }
-      })
-      AppController.instance?.addToRequestQueue(quoteRequest)
-  }
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+            if (true) callback.processFinished(quoteArrayList)
+        }, Response.ErrorListener { error: VolleyError ->
+            try {
+                Log.d("Error", "Not Working")
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        })
+        AppController.instance?.addToRequestQueue(quoteRequest)
+    }
 }
